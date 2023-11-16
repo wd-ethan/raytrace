220f984d678ba1de608bdf08958b37d3350fc80a
Ethan Wright <me@wrighte.dev>
2023-11-15 20:34:59 -0800

    inital stubs

---
+++ RayTracer.java
@@ -0,0 +1,12 @@
+public class RayTracer {
+
+    RayTracer(final String filePath) {
+        filepath = filePath;
+    }
+
+    String filepath;
+
+    public trace() {
+        // main loop goes here.
+    }
+}
